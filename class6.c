208afdf8739f0176b75fc69fcdb26d8f2b24e1f3
parzuko <jivansh17@gmail.com>
2022-09-22 13:40:32 +0530

    🏗 update directory structure

---
+++ class6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    float answer = 0.1 + 0.2;
+    
+    if (answer == 0.3) {
+        printf("The answer is 0.3");
+    } else {
+        printf("The program broke :)");
+    }
+}
